88c874f1e09ee1ed648e6d9729cc54f765f1376a
MoatazGhabri <120782597+MoatazGhabri@users.noreply.github.com>
2026-01-23 14:47:21 +0100

    api

--- server/src/verify_migration.ts
+++ server/src/verify_migration.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'http://31.97.177.87/api';
 
 async function verify() {
     console.log('Verifying API Health...');
